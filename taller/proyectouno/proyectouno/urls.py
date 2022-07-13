3cbba56f311587d2383a6f03df018c49af9c0db0
Carlos Tandazo <carlosatandazo@gmail.com>
2022-07-12 19:32:19 -0500

    taller13

--- taller/proyectouno/proyectouno/urls.py
+++ taller/proyectouno/proyectouno/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include# url
 
 from rest_framework import routers
 from administrativo import views
